3c6fd21139a53a6f43da99b7325a4480660b94d8
Arafat99974 <arafatas602@gmail.com>
2025-04-03 09:58:46 +0600

    Update prefix2.js

--- scripts/cmds/prefix2.js
+++ scripts/cmds/prefix2.js
@@ -17,10 +17,10 @@ module.exports = {
  body: `
 Yo, my prefix is [ 𓆩 ( 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ 𝙊𝙒𝙉𝙀𝙍: ~𝗘𝘄𝗿 𝗦𝗵𝗔𝗻'𝘀
+➥ 𝙊𝙒𝙉𝙀𝙍: ~𝐀𝐫𝐚𝐟𝐚𝐭
 ➥ ✓sim [bby] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
-➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️\nBot Developer:https://www.facebook.com/sirana252`,
+➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️\nBot Developer:https://www.facebook.com/Arafatas602`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
